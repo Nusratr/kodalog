a00ff5397107d0b3dc15c207bc725185a6b98614
kipcakbegi <kipcakbegi@gmail.com>
2016-08-20 00:15:18 +0300

    Update kodalog.js

--- kodalog.js
+++ kodalog.js
@@ -1,5 +1,4 @@
 document.querySelector('.kodalog').addEventListener("click", function(){
-		var kClass = document.querySelector('.kodalog-console').id;
-		document.querySelector('.kodalog-console').id = kClass == "" ? 
-"etkin" : "";
+	var kClass = document.querySelector('.kodalog-console').id;
+	document.querySelector('.kodalog-console').id = kClass == "" ? "etkin" : "";
 }, true);
