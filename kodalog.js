c481619f95e68eccbf36ef5af89d87604d4642d9
Avare Kodcu <abdurrahmaneker58@gmail.com>
2016-08-20 15:10:48 +0300

    Tasarım Sabit Hale Getirildi.

--- kodalog.js
+++ kodalog.js
@@ -17,9 +17,12 @@
 	};
 
 	window.onload = function () {
-		document.querySelector('.kodalog').addEventListener("click", function(){
-			var kClass = document.querySelector('.kodalog-console').id;
-			document.querySelector('.kodalog-console').id = kClass == "" ? "etkin" : "";
+		document.querySelector('.kodalog-icon').addEventListener("click", function() {
+			var kClass = document.querySelector('.kodalog').id;
+			document.querySelector('.kodalog').id = kClass == "" ? "activeKodalog" : "";
+		}, true);
+		document.querySelector('.kl-console').addEventListener("click", function () {
+			document.getElementById('klConsole').focus();
 		}, true);
 	};
 })();
