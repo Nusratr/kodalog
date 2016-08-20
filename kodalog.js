8067562818d1dcee7bca760ec56d2ae9e2ae54d0
kipcakbegi <kipcakbegi@gmail.com>
2016-08-20 21:56:56 +0300

    Ekrana bastırma işlemi eklendi.

--- kodalog.js
+++ kodalog.js
@@ -1,21 +1,33 @@
-/*
- * Kodalog Javascript Console Plugin
- * Start : 2016-08-19 23:41
-*/
-
 (function() {
-	window.onerror = function(err) {
-		kodalog(err);
+	function newError(err, url, lineNum){
+		var klConsoleDiv = document.querySelector(".kl-console");
+		var newErr = document.createElement("div");
+		newErr.className = "kl-console-error";
+		newErr.innerHTML = "<p>"+err+"</p><span>"+url+":"+lineNum+"</span>"; 
+		klConsoleDiv.appendChild(newErr);
+	}
+	function newLog(msg){
+		var klConsoleDiv = document.querySelector(".kl-console");
+		var newErr = document.createElement("div");
+		newErr.className = "kl-console-log";
+		newErr.innerHTML = "<p>"+msg+"</p><span></span>"; 
+		klConsoleDiv.appendChild(newErr);
+	}
+	window.onerror = function(err, url, lineNum) {
+		kodalog(err, url, lineNum);
 	};
 	nativeLog = console.log.bind(console);
-	console.log = function (obj) {
+	console.log = function(obj, url, lineNum) {
 		nativeLog(obj);
-		kodalog(obj);
+		kodalog(obj, url, lineNum);
 	};
-	var kodalog = function(obj) {
-		alert(obj);	
+	var kodalog = function(obj, url, lineNum) {
+		if(url && lineNum){ // eğer hata ise
+			newError(obj, url, lineNum);
+		}else{
+			newLog(obj);
+		}
 	};
-
 	window.onload = function () {
 		document.querySelector('.kodalog-icon').addEventListener("click", function() {
 			var kClass = document.querySelector('.kodalog').id;
